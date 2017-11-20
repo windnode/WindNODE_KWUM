f6283f50951afe61ea2da74e499acc9bbbc5e8be
nesnoj <jonathan.amme@rl-institut.de>
2017-11-20 06:16:41 +0100

    extend oep example

--- windnode_kwum/examples/get_data_from_oep.py
+++ windnode_kwum/examples/get_data_from_oep.py
@@ -14,11 +14,18 @@ config.load_config('config_misc.cfg')
 from windnode_kwum.tools.data import oep_get_data
 
 
+# get Kreis UM
+kreis = oep_get_data(schema='boundaries',
+                     table='bkg_vg250_4_krs',
+                     columns=['id', 'geom'],
+                     conditions=['nuts=DE40I'])
+print(kreis)
+
+# get Load Areas
 demand = oep_get_data(schema='demand',
                      table='ego_dp_loadarea',
                      columns=['id',
                               'zensus_sum',
                               'sector_consumption_sum'],
                       conditions=['version=v0.3.0pre1', 'subst_id=1'])
-
 print(demand)
