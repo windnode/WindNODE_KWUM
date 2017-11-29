c544b61ce977377e421e440e5834eb0481150b42
nesnoj <jonathan.amme@rl-institut.de>
2017-11-29 09:46:49 +0100

    add example script and example scenario Excel file for simulation of basic KWUM model

---
+++ windnode_kwum/scenarios/example_scenario.py
@@ -0,0 +1,106 @@
+# define and setup logger
+from windnode_kwum.tools.logger import setup_logger
+logger = setup_logger()
+
+import os
+from windnode_kwum.model.KWUM_basic import create_model, simulate
+from windnode_kwum.tools import config
+config.load_config('config_data.cfg')
+config.load_config('config_misc.cfg')
+
+# import oemof modules
+import oemof.solph as solph
+from oemof import outputlib
+
+import matplotlib.pyplot as plt
+
+
+def run_scenario(cfg):
+
+    esys = create_model(cfg=cfg)
+
+    results = simulate(esys=esys,
+                       solver=cfg['solver'])
+
+    if cfg['dump']:
+        path = os.path.join(config.get_data_root_dir(),
+                            config.get('user_dirs',
+                                       'results_dir')
+                            )
+        file = os.path.splitext(os.path.basename(__file__))[0] + '.oemof'
+
+        esys.dump(dpath=path,
+                  filename=file)
+        logger.info('The energy system was dumped to {}.'
+                    .format(path + file))
+
+    # else:
+    #     esys = solph.EnergySystem()
+    #     esys.restore(dpath=os.path.join(config.get_data_root_dir(),
+    #                                     config.get('user_dirs',
+    #                                                'results_dir')
+    #                                     ),
+    #                  filename='esys_example_scenario.oemof')
+
+
+
+
+
+    # my_results = bus_el['sequences'].sum(axis=0).to_dict()
+    # my_results['storage_invest'] = results[(storage,)]['scalars']['invest']
+
+    return esys, results
+
+
+def plot_results(esys, results):
+
+    logger.info('Plot results')
+
+    from oemof.outputlib.graph_tools import graph
+    graph(esys)
+
+    bus_el = esys.groups['bus_el']
+    bus_el_results = outputlib.views.node(results, 'bus_el')
+
+    print(bus_el_results['sequences'].sum())
+    print(bus_el_results['sequences'].info())
+
+    bus_el_results['sequences'].plot(kind='line', drawstyle='steps-post')
+    plt.show()
+
+    ax = bus_el_results['sequences'].sum(axis=0).plot(kind='barh')
+    ax.set_title('Sums for optimization period')
+    ax.set_xlabel('Energy (MWh)')
+    ax.set_ylabel('Flow')
+    plt.tight_layout()
+    plt.show()
+
+    # data = results[(bus_el,)]['sequences']
+    # ax = data.plot(kind='scatter', x='Q', y='P', grid=True)
+    # ax.set_xlabel('Q (MW)')
+    # ax.set_ylabel('P (MW)')
+    # plt.show()
+
+if __name__ == "__main__":
+
+    # configuration
+    cfg = {
+        'data_path': os.path.join(os.path.dirname(__file__), 'data'),
+        'date_from': '2013-01-01 00:00:00',
+        'date_to': '2013-01-07 23:00:00',
+        'freq': '60min',
+        'scenario_file': 'example_scenario.xlsx',
+        'results_path': os.path.join(config.get_data_root_dir(),
+                                     config.get('user_dirs',
+                                                'results_dir')),
+        'solver': 'cbc',
+        'verbose': True,
+        'dump': True
+    }
+
+    esys, results = run_scenario(cfg=cfg)
+
+    plot_results(esys=esys,
+                 results=results)
+
+    logger.info('Done!')
