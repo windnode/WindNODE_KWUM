9b8b0bf373cc27621c3f008bee17dd983c4104f4
nesnoj <jonathan.amme@rl-institut.de>
2018-01-24 14:22:01 +0100

    adapted to oemof v0.2.0, solves #4

--- windnode_kwum/scenarios/basic_scenario.py
+++ windnode_kwum/scenarios/basic_scenario.py
@@ -9,8 +9,10 @@ config.load_config('config_data.cfg')
 config.load_config('config_misc.cfg')
 
 # import oemof modules
-from oemof import outputlib
+from oemof.outputlib import processing, views
+from oemof.graph import create_nx_graph
 
+import networkx as nx
 import matplotlib.pyplot as plt
 
 
@@ -59,39 +61,111 @@ def plot_results(esys, results):
         Results of simulation
     """
 
+    def draw_graph(grph, edge_labels=True, node_color='#AFAFAF',
+                   edge_color='#CFCFCF', plot=True, node_size=2000,
+                   with_labels=True, arrows=True, layout='neato'):
+        """
+        Draw a graph (from oemof examples)
+
+        Parameters
+        ----------
+        grph : networkxGraph
+            A graph to draw.
+        edge_labels : boolean
+            Use nominal values of flow as edge label
+        node_color : dict or string
+            Hex color code oder matplotlib color for each node. If string, all
+            colors are the same.
+
+        edge_color : string
+            Hex color code oder matplotlib color for edge color.
+
+        plot : boolean
+            Show matplotlib plot.
+
+        node_size : integer
+            Size of nodes.
+
+        with_labels : boolean
+            Draw node labels.
+
+        arrows : boolean
+            Draw arrows on directed edges. Works only if an optimization_model has
+            been passed.
+        layout : string
+            networkx graph layout, one of: neato, dot, twopi, circo, fdp, sfdp.
+        """
+        if type(node_color) is dict:
+            node_color = [node_color.get(g, '#AFAFAF') for g in grph.nodes()]
+
+        # set drawing options
+        options = {
+            'prog': 'dot',
+            'with_labels': with_labels,
+            'node_color': node_color,
+            'edge_color': edge_color,
+            'node_size': node_size,
+            'arrows': arrows
+        }
+
+        # draw graph
+        pos = nx.drawing.nx_agraph.graphviz_layout(grph, prog=layout)
+
+        nx.draw(grph, pos=pos, **options)
+
+        # add edge labels for all edges
+        if edge_labels is True and plt:
+            labels = nx.get_edge_attributes(grph, 'weight')
+            nx.draw_networkx_edge_labels(grph, pos=pos, edge_labels=labels)
+
+        # show output
+        if plot is True:
+            plt.show()
+
     logger.info('Plot results')
 
     # print graph of energy system
-    from oemof.outputlib.graph_tools import graph
-    graph(esys, node_size=1000)
+    graph = create_nx_graph(esys)
+    draw_graph(grph=graph, plot=True, layout='neato', node_size=1000,
+               node_color={
+                   'bus_el': '#cd3333',
+                   'bus_gas': '#7EC0EE',
+                   'bus_th': '#eeac7e'})
 
-    # get
-    bus_el = esys.groups['bus_el']
-    bus_el_results = outputlib.views.node(results, 'bus_el')
-    bus_th_results = outputlib.views.node(results, 'bus_th')
-    bus_th_prenzlau_results = outputlib.views.node(results, 'bus_th_prenzlau')
-    bus_th_nechlin_results = outputlib.views.node(results, 'bus_th_nechlin')
+    # get bus_el and bus_th data from results
+    bus_el_results = views.node(results, 'bus_el')
+    bus_el_results_flows = bus_el_results['sequences']
+    bus_th_results = views.node(results, 'bus_th')
+    bus_th_results_flows = bus_th_results['sequences']
 
+    # print some sums for bus_el
     print(bus_el_results['sequences'].sum())
     print(bus_el_results['sequences'].info())
 
-    bus_el_results['sequences'].plot(kind='line', drawstyle='steps-post')
-    plt.show()
-
-    ax = bus_el_results['sequences'].sum(axis=0).plot(kind='barh')
+    # some example plots for bus_el
+    ax = bus_el_results_flows.sum(axis=0).plot(kind='barh')
     ax.set_title('Sums for optimization period')
     ax.set_xlabel('Energy (MWh)')
     ax.set_ylabel('Flow')
     plt.tight_layout()
     plt.show()
 
-    # data = results[(bus_el,)]['sequences']
-    # ax = data.plot(kind='scatter', x='Q', y='P', grid=True)
-    # ax.set_xlabel('Q (MW)')
-    # ax.set_ylabel('P (MW)')
-    # plt.show()
+    bus_el_results_flows.plot(kind='line', drawstyle='steps-post')
+    plt.show()
 
-    bus_th_results['sequences'].plot(kind='line', drawstyle='steps-post')
+    ax = bus_el_results_flows.plot(kind='bar', stacked=True, linewidth=0, width=1)
+    ax.set_title('Sums for optimization period')
+    ax.legend(loc='upper right', bbox_to_anchor=(1, 1))
+    ax.set_xlabel('Energy (MWh)')
+    ax.set_ylabel('Flow')
+    plt.tight_layout()
+
+    dates = bus_el_results_flows.index
+    tick_distance = int(len(dates) / 7) - 1
+    ax.set_xticks(range(0, len(dates), tick_distance), minor=False)
+    ax.set_xticklabels(
+        [item.strftime('%d-%m-%Y') for item in dates.tolist()[0::tick_distance]],
+        rotation=90, minor=False)
     plt.show()
 
 if __name__ == "__main__":
@@ -100,7 +174,7 @@ if __name__ == "__main__":
     cfg = {
         'data_path': os.path.join(os.path.dirname(__file__), 'data'),
         'date_from': '2016-01-01 00:00:00',
-        'date_to': '2016-01-15 23:00:00',
+        'date_to': '2016-01-07 23:00:00',
         'freq': '60min',
         'scenario_file': 'basic_scenario.xlsx',
         'data_file': 'basic_scenario_data.xlsx',
