2b79490845667ab1248231a25859941d85ef738a
nesnoj <jonathan.amme@rl-institut.de>
2018-01-24 14:49:24 +0100

    move graph plotting function to tools/draw.py

--- windnode_kwum/scenarios/basic_scenario.py
+++ windnode_kwum/scenarios/basic_scenario.py
@@ -7,12 +7,12 @@ from windnode_kwum.models.basic_model import create_model, simulate
 from windnode_kwum.tools import config
 config.load_config('config_data.cfg')
 config.load_config('config_misc.cfg')
+from windnode_kwum.tools.draw import draw_graph
 
 # import oemof modules
 from oemof.outputlib import processing, views
 from oemof.graph import create_nx_graph
 
-import networkx as nx
 import matplotlib.pyplot as plt
 
 
@@ -60,67 +60,6 @@ def plot_results(esys, results):
     results : :obj:`dict`
         Results of simulation
     """
-
-    def draw_graph(grph, edge_labels=True, node_color='#AFAFAF',
-                   edge_color='#CFCFCF', plot=True, node_size=2000,
-                   with_labels=True, arrows=True, layout='neato'):
-        """
-        Draw a graph (from oemof examples)
-
-        Parameters
-        ----------
-        grph : networkxGraph
-            A graph to draw.
-        edge_labels : boolean
-            Use nominal values of flow as edge label
-        node_color : dict or string
-            Hex color code oder matplotlib color for each node. If string, all
-            colors are the same.
-
-        edge_color : string
-            Hex color code oder matplotlib color for edge color.
-
-        plot : boolean
-            Show matplotlib plot.
-
-        node_size : integer
-            Size of nodes.
-
-        with_labels : boolean
-            Draw node labels.
-
-        arrows : boolean
-            Draw arrows on directed edges. Works only if an optimization_model has
-            been passed.
-        layout : string
-            networkx graph layout, one of: neato, dot, twopi, circo, fdp, sfdp.
-        """
-        if type(node_color) is dict:
-            node_color = [node_color.get(g, '#AFAFAF') for g in grph.nodes()]
-
-        # set drawing options
-        options = {
-            'prog': 'dot',
-            'with_labels': with_labels,
-            'node_color': node_color,
-            'edge_color': edge_color,
-            'node_size': node_size,
-            'arrows': arrows
-        }
-
-        # draw graph
-        pos = nx.drawing.nx_agraph.graphviz_layout(grph, prog=layout)
-
-        nx.draw(grph, pos=pos, **options)
-
-        # add edge labels for all edges
-        if edge_labels is True and plt:
-            labels = nx.get_edge_attributes(grph, 'weight')
-            nx.draw_networkx_edge_labels(grph, pos=pos, edge_labels=labels)
-
-        # show output
-        if plot is True:
-            plt.show()
 
     logger.info('Plot results')
 
