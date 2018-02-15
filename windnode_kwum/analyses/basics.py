aed6775baf8dce87a8d9b5693c61eed1a78ec299
Elisa Förster <Elisa.Foerster@rl-institut.de>
2018-02-15 16:59:18 +0100

    add basic functions for analyses

---
+++ windnode_kwum/analyses/basics.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 14 08:35:53 2018
+
+@author: Elisa.Foerster
+"""
+
+from windnode_kwum.tools import config
+import os
+import oemof.solph as solph
+from oemof.outputlib import views
+
+
+def get_flow(label, inout, i=0):
+    """get in- or outputflow of a component
+
+    Parameters
+    ----------
+    label : string
+        unique name of the component
+    inout : string
+        in: inputflow
+        out: outputflow
+    i : integer
+        in case of n in- or outputs; default = 0
+    Returns
+    -------
+    flow : `obj`
+    """
+    # get componenet:
+    comp = [i for i in esys.entities if i.label == label][0]
+    # get (first) input-bus-object for component:
+    if inout == 'in':
+        a = list(comp.inputs.keys())[i]
+    else:
+        a = list(comp.outputs.keys())[i]
+    # get input-flow from component:
+    flow = comp.inputs[a]
+
+    return flow
+
+
+def get_energy_flow_sequence(results, bus, from_comp, to_comp):
+    """get in- or output sequence of a components energy flow
+
+    Parameters
+    ----------
+    results : dataframe(?) results
+    bus : string (name of bus)
+    from_comp : string (name of component, where the energy comes from)
+    to_comp : string (name of component, where the energy goes to)
+    Returns
+    -------
+    sequence of nergy flow
+    """
+    bus_results = views.node(results, bus)
+    bus_results_flows = bus_results['sequences']
+    energy_flow_sequence = bus_results_flows[((from_comp,to_comp),'flow')]
+
+    return energy_flow_sequence
+
+
+# get data from scenario
+scenario_name = 'reference_scenario_curtailment'
+
+# get results_path
+path = os.path.join(config.get_data_root_dir(),
+                    config.get('user_dirs',
+                               'results_dir')
+                    )
+
+# restore energysystem
+esys = solph.EnergySystem()
+file = scenario_name + '.oemof'
+esys.restore(dpath=path,
+             filename=file)
+
+results = esys.results
+
+############### analyse general results:
+bus_el_results = views.node(results, 'bus_el')
+bus_el_results_flows = bus_el_results['sequences']
+
+# get timeindex of sequences
+tidx = bus_el_results_flows.index
+
+energy_sums = bus_el_results_flows.sum(axis=0)
+
+
+############### analyse special results
+# get spot_market inflow sequence :
+# spot_market_in = bus_el_results_flows[(('bus_el','spot_market'),'flow')]
+# alternativ:
+spot_market_in = get_energy_flow_sequence(
+                    results, 'bus_el', 'bus_el','spot_market')
+
+# get flow object of spotmarket_inflow (in order to read out variable_costs)
+spot_market_inflow = get_flow('spot_market', 'in')
+# get variable_costst of inflow (with timeindex of simulation)
+spot_market_variable_costs = spot_market_inflow.variable_costs[tidx]
+
+spot_market_prices = - spot_market_variable_costs
+spot_market_revenues = spot_market_in * spot_market_prices
+revenuesum = spot_market_revenues.sum()
+
+# TODO: implement calculation of compensation for curtailment
+# curtailment = bus_el_results_flows[(('bus_el','curtailment'),'flow')]
