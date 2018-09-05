16bd00cbe153c41e5f037bae86c34f09dd6f0b54
nesnoj <jonathan.amme@rl-institut.de>
2018-09-05 11:22:16 +0200

    add version no to docs

--- doc/conf.py
+++ doc/conf.py
@@ -24,7 +24,7 @@ copyright = '2018, Reiner Lemoine Institut gGmbH'
 author = 'Reiner Lemoine Institut gGmbH'
 
 # The short X.Y version
-version = ''
+version = '0.0.1'
 # The full version, including alpha/beta/rc tags
 release = ''
 
