62d94a29bf383771c884ab01a3b2e3da1915f5ff
nesnoj <jonathan.amme@rl-institut.de>
2018-09-05 12:10:37 +0200

    adjust napoleon config

--- doc/conf.py
+++ doc/conf.py
@@ -61,7 +61,7 @@ napoleon_use_admonition_for_references = False
 napoleon_use_ivar = False
 napoleon_use_param = True
 napoleon_use_rtype = True
-napoleon_use_keyword = True
+napoleon_use_keyword = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
