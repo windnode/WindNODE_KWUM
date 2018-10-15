f7d3b1909476230346aeca8c24aea4a657d4bdc4
Pamela Garcia Romero <Pamela.Romero@rl-institut.de>
2018-10-15 17:35:56 +0200

    Change parameters-to-vary  in the excel file: - Define the parameters-to-vary and min, max, step - Change the parameters in the excel sheet "reference_scenario_curtailment.xlsx" - Run the reference_scenario_curtailment Main

--- windnode_kwum/scenarios/sensitivity_analysis.py
+++ windnode_kwum/scenarios/sensitivity_analysis.py
@@ -1,9 +1,7 @@
 import itertools
 import numpy as np
-import os
 from windnode_kwum.scenarios import reference_scenario_curtailment
 from openpyxl import load_workbook
-import csv
 
 # function to create combinations of param values while preserving param names
 def product_dict(**kwargs):
@@ -13,7 +11,7 @@ def product_dict(**kwargs):
         yield dict(zip(keys, instance))
 
 # format: 'param': [min, max, step]
-params_to_be_varied = {'transformer.nombre': [1, 68, 0.5], 'param2': [1, 12, 0.5]}
+params_to_be_varied = {'transformers.P2H_sch.capacity': [10, 12, 1], 'chp.chp_sch.power max':[1, 2, 1]}
 
 # create ranges
 param_val_ranges = {}
@@ -27,37 +25,44 @@ param_val_combinations = list(product_dict(**param_val_ranges))
 for run_no, comb in enumerate(param_val_combinations):
     print('Run no ', str(run_no))
     print('=============')
+    parameter_set = [];
     for key, val in comb.items():
         print('Parameter', key, '=', val)
-        # INSERT MODEL PARAMETERIZATION HERE
 
-        # scenario_file = os.path.join('reference_scenario_curtailment.xlsx')
-        dest = '/Users/ricardoviteribuendia/Desktop/Paytoncosas/Repositories/WindNODE_KWUM/windnode_kwum/scenarios/data/reference_scenario_curtailment.xlsx'
-        # Open an xlsx for reading
-        wb = load_workbook(filename=dest)
-        # Get the current Active Sheet
-        # ws = wb.get_active_sheet()
+        parameter_set.append({'key' : key, 'value': str(val)})
+
+
+    # INSERT MODEL PARAMETERIZATION HERE
+
+    # scenario_file = os.path.join('reference_scenario_curtailment.xlsx')
+   #TODO adjust the file path!!!
+    dest = '/Users/ricardoviteribuendia/Desktop/Paytoncosas/Repositories/WindNODE_KWUM/windnode_kwum/scenarios/data/reference_scenario_curtailment.xlsx'
+
+    # Open an xlsx for reading
+    wb = load_workbook(filename=dest)
+
+    for param in parameter_set:
+
+        file_sections = param.get('key').split('.')
+
         # You can also select a particular sheet
         # based on sheet name
-        ws = wb.get_sheet_by_name("transformers")
-        # Open the csv file
-        with open(dest) as fin:
-            # read the csv
-            reader = csv.reader(fin)
-            # enumerate the rows, so that you can
-            # get the row index for the xlsx
-            for index, row in enumerate(reader):
-                # Assuming space separated,
-                # Split the row to cells (column)
-                row = row[0].split()
-                # Access the particular cell and assign
-                # the value from the csv row
-                ws.cell(row=index, column=7).value = row[2]
-                ws.cell(row=index, column=8).value = row[3]
-        # save the csb file
-        wb.save(dest)
+        ws = wb.get_sheet_by_name(file_sections[0])
 
+        row_to_edit = ''
+        column_to_edit = ''
 
-        reference_scenario_curtailment.executeMain()
+        for cell in ws.get_cell_collection():
+            if cell.value == file_sections[1]:
+                row_to_edit = cell.row
 
+            if cell.value == file_sections[2]:
+                column_to_edit = cell.col_idx
 
+        ws.cell(row=row_to_edit, column=column_to_edit).value = param.get('value')
+
+    wb.save(dest)
+
+    reference_scenario_curtailment.executeMain()
+
+    print('======== Run END ========')
