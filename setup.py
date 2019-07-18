c62f4f50f8248f52265ffe529f17b6bc4a538480
schluzkofski <39306114+schluzkofski@users.noreply.github.com>
2019-07-18 17:28:00 +0200

    updated setup file

--- setup.py
+++ setup.py
@@ -38,7 +38,10 @@ setup(
         #'pypsa >=0.10.0, <=0.10.0',
         'pyproj >=1.9.5.1',
         'requests >=2.18.4',
-        'xlrd >=0.1.1', 'openpyxl'
+        'xlrd >=0.1.1',
+        'openpyxl >=2.5.8',
+        'pypiwin32 >=219',
+        'matplotlib	>=2.2.3'
         #'geopy >= 1.11.0, <= 1.11.0'
     ],
     package_data={
